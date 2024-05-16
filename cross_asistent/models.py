8ccaf87b61e7c18bac780c7daa001f7b998cf9bf
Stranger11ac <salvahdzcep@gmail.com>
2024-05-15 19:56:19 -0600

    create models (tables) and create superuser for salva

--- cross_asistent/models.py
+++ cross_asistent/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class proyectos(models.Model):
+    nombre = models.CharField(max_length=150)
+
+class tareas(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    proyecto = models.ForeignKey(proyectos, on_delete=models.CASCADE)
--- cross_project/settings.py
+++ cross_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cross_asistent',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
