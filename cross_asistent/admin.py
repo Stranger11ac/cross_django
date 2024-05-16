efa33ee785355c75446b826dfdf9c2124ae16044
Stranger11ac <salvahdzcep@gmail.com>
2024-05-15 23:12:35 -0600

    create models: banners and articulos

--- cross_asistent/admin.py
+++ cross_asistent/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-# from .models import proyectos, tareas, preguntas
 from . import models
+# from .models import proyectos, tareas, preguntas
 
 # Register your models here.
-admin.site.register(models.proyectos)
+admin.site.register(models.banners)
 admin.site.register(models.tareas)
+admin.site.register(models.proyectos)
 admin.site.register(models.preguntas)
+admin.site.register(models.articulos)
--- cross_asistent/models.py
+++ cross_asistent/models.py
@@ -18,16 +18,30 @@ class tareas(models.Model):
 class preguntas(models.Model):
     pregunta = models.CharField(max_length=200, blank=False)
     respuesta = models.TextField(blank=False)
-    redirigir = models.URLField(blank=True)
-    documentos = models.FileField(upload_to='referencias/documentos/', blank=True)
-    imagenes =  models.ImageField(upload_to='referencias/imagenes/', blank=True)
+    redirigir = models.URLField(blank=True, null=True)
+    documentos = models.FileField(upload_to='referencias/documentos/', blank=True, null=True)
+    imagenes =  models.ImageField(upload_to='referencias/imagenes/', blank=True, null=True)
     fecha_modificacion = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.pregunta
+    
+class articulos(models.Model):
+    creacion = models.DateField(auto_now_add=True, blank=False)
+    actualizacion = models.DateField(auto_now=True, blank=True, null=True)
+    titulo = models.CharField(max_length=200, blank=False)
+    contenido = models.TextField(blank=False)
+    autor = models.CharField(max_length=150, blank=False)
+    
+    def __str__(self):
+        return self.titulo + " - " + self.autor
 
 class banners(models.Model):
     titulo = models.CharField(max_length=150, blank=False)
     descripcion = models.CharField(max_length=200, blank=False)
+    articulo = models.URLField(blank=False, default=False)
     imagen = models.ImageField(upload_to='referencias/banners', blank=False)
-    expiracion = models.DateTimeField(blank=True)
+    expiracion = models.DateTimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.titulo
