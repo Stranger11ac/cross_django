e4ae501cc36ca48d048eda278c8f19f3b64b26ac
Stranger11ac <salvahdzcep@gmail.com>
2024-05-21 13:59:49 -0600

    Add input for chat in index

--- cross_asistent/static/js/settings.js
+++ cross_asistent/static/js/settings.js
@@ -3,6 +3,19 @@ document.oncontextmenu = function () {
     return false;
 };
 
+$(document).ready(function () {
+    // alertSToast('center', 8000, 'info', camera_orbit);
+    // $('#asistent_model').attr('camera-orbit', camera_orbit);
+    const modelViewer = $("#asistent_model");
+    const camera_orbit = modelViewer.attr("camera-orbit");
+
+    modelViewer.on("loaded", function () {
+        setTimeout(function () {
+            modelViewer.attr("camera-orbit", camera_orbit);
+        }, 3000);
+    });
+});
+
 // Template Alerta switalert ################################
 function alertSToast(posittionS, timerS, iconS, titleS, didCloseS) {
     const Toast = Swal.mixin({
@@ -32,6 +45,5 @@ function alertSToast(posittionS, timerS, iconS, titleS, didCloseS) {
         title: titleS,
     });
 }
-
 
 // alertSToast('top', 8000, 'success', '<br>lo normal');
