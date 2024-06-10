0f11a56b83121a9c10ae2ea0a6b75e01a51405b2
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2024-06-10 11:52:03 -0600

    update repository

--- cross_asistent/urls.py
+++ cross_asistent/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('administracion/registro/', views.singuppage, name='singup'),
     path('administracion/', views.dashbAdmin, name='dashb_admin'),
     path('administracion/tarea/<int:tarea_id>/', views.tareaView, name='tareaView'),
-    path('chat/', views.chat_view, name='chat'),
 # se agrego la url para la vista administrador
     path('administracion/vista_admin/', views.vista_admin, name='vista_admin'),
     path('administracion/vista_programador/', views.vista_programador, name='vista_programador'),
