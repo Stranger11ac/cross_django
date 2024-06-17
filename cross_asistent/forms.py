037d185d44e5d86a0032f4c71e006a8bd2c97963
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2024-06-16 23:52:53 -0600

    detele forms for map create information

--- cross_asistent/forms.py
+++ cross_asistent/forms.py
@@ -1,24 +1,12 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Tareas, Mapa, MapaImagenes
+from .models import Tareas
 from .models import Database, Categorias
 
 class crearTarea(ModelForm):
     class Meta:
         model = Tareas
         fields = ['tarea', 'importante']
-
-class MapaForm(forms.ModelForm):
-    class Meta:
-        model = Mapa
-        fields = ['lugar', 'informacion']
-
-class MapaImagenesForm(forms.ModelForm):
-    class Meta:
-        model = MapaImagenes
-        fields = ['imagen']
-
-    imagen = forms.ImageField(widget=forms.ClearableFileInput())
 
 class PreguntaForm(forms.ModelForm):
     class Meta:
