37653fcc783dcf698f72fd12340e811e6e406918
Josue0Cg <josueozemcg@gmail.com>
2024-06-27 15:37:34 -0600

    update the chatbot by making an initial greeting

--- cross_asistent/views.py
+++ cross_asistent/views.py
@@ -27,6 +27,40 @@ import csv
 import os
 from django.core.files import File
 
+# openai.api_key = ""
+
+# chat_history = []
+
+# while True:
+#     prompt = input("Enter a prompt: ")
+#     if prompt == "exit":
+#         break
+#     else:
+#         chat_history.append({"role": "user", "content": prompt})
+
+#         response_iterator = openai.ChatCompletion.create(
+#             model="gpt-3.5-turbo",
+#             messages = chat_history,
+#             stream=True,
+#             max_tokens=150,
+#         )
+
+#         collected_messages = []
+
+#         for chunk in response_iterator:
+#             chunk_message = chunk['choices'][0]['delta']  # extract the message
+#             collected_messages.append(chunk_message)  # save the message
+#             full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+#             print(full_reply_content)
+
+#             # clear the terminal
+#             print("\033[H\033[J", end="")
+
+#         chat_history.append({"role": "assistant", "content": full_reply_content})
+#         # print the time delay and text received
+#         full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+#         print(f"GPT: {full_reply_content}")
+
 
 def index(request):
     if not request.user.is_staff:
