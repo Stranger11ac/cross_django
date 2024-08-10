100afcbfafe3ccbfb7158c36feef9b4c63caccc6
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2024-08-10 08:46:21 -0600

    optifine functions fullcalendar

--- cross_asistent/static/js/fullcalendar.js
+++ cross_asistent/static/js/fullcalendar.js
@@ -57,95 +57,69 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         eventClick: function (info) {
             var eventObj = info.event;
-            const imgJson = eventObj.extendedProps.imagen;
+            var {end: valEnd,start: valStart,title: valTitulo,allDay: valallDay,classNames: valclassNames,} = eventObj;
+            var {imagen: imgJson,button: valBtn,location: valPleace,description: valDescription,} = eventObj.extendedProps;
             var myModal = new mdb.Modal(document.getElementById("eventModal"));
-            const valEnd = eventObj.end;
-            const valStart = eventObj.start;
-            const valTitulo = eventObj.title;
-            const valallDay = eventObj.allDay;
-            const valclassNames = eventObj.classNames;
-            const valBtn = eventObj.extendedProps.button;
-            const valPleace = eventObj.extendedProps.location;
-            const valDescription = eventObj.extendedProps.description;
+            const updateModal = $("#eventModal").hasClass("calendar_update");
 
-            if ($("#eventModal").hasClass("calendar_update")) {
+            if (updateModal) {
                 $(".idUpdate").val(eventObj.id);
 
-                $("#tituloUpdate").addClass("active").val(valTitulo);
+                $("#tituloUpdate, #informacionUpdate, #redirigirUpdate, #ePleaceUpdate").each(function () {
+                    $(this)
+                        .addClass("active")
+                        .val(eval(this.id.replace("Update", "")));
+                });
+
                 $(".eventTitle").text(valTitulo);
 
-                $("#informacionUpdate").addClass("active").val(valDescription);
-                $("#redirigirUpdate").addClass("active").val(valBtn);
-                $("#ePleaceUpdate").addClass("active").val(valPleace);
                 $("#eStartUpdate")
                     .addClass("active")
                     .val(`${formatDateInput(valStart)}T${formatTimeInput(valStart)}`);
-                if (valEnd) {
-                    $("#eEndUpdate")
-                        .addClass("active")
-                        .val(`${formatDateInput(valEnd)}T${formatTimeInput(valEnd)}`);
-                } else {
-                    $("#eEndUpdate")
-                        .addClass("active")
-                        .val(`${formatDateInput(valStart)}T${formatTimeInput(valStart)}`);
-                }
-                if (valallDay) {
-                    $("#eAllDayUpdate").attr("checked", true);
-                } else {
-                    $("#eAllDayUpdate").attr("checked", false);
-                }
-                $("#eColorUpdate option#eColorSelected").attr("selected", false);
-                $(`#eColorUpdate option[value="${valclassNames}"]`).attr("selected", true);
+                $("#eEndUpdate")
+                    .addClass("active")
+                    .val(`${formatDateInput(valEnd || valStart)}T${formatTimeInput(valEnd || valStart)}`);
+                $("#eAllDayUpdate").prop("checked", valallDay);
+
+                $("#eColorUpdate option").prop("selected", false);
+                $(`#eColorUpdate option[value="${valclassNames}"]`).prop("selected", true);
                 $("[data-select_addClass]").attr("class", `form-select change_bg ${valclassNames}`);
 
-                if (imgJson == "") {
-                    $("[for='imagenUpdate']").html('Subir Imagen <i class="fa-regular fa-images ms-1"></i>');
-                } else {
-                    $("[for='imagenUpdate']").html('Cambiar Imagen <i class="fa-regular fa-images ms-1"></i>');
-                }
+                const imgLabel = imgJson === "" ? "Subir Imagen" : "Cambiar Imagen";
+                $("[for='imagenUpdate']").html(`${imgLabel} <i class="fa-regular fa-images ms-1"></i>`);
+                
             } else {
                 $("#eventModalLabel").text(valTitulo);
                 $("#eventStartDate").text(formatDate(valStart));
                 $("#eventStartTime").text(formatTime(valStart));
                 $("#eventLoc").text(valPleace || "Campus UTC");
+
                 if (valDescription) {
-                    $("#eventDesc").text(valDescription);
+                    $("#eventDesc").text(valDescription).removeClass("none");
                 } else {
                     $("#eventDesc").addClass("none");
                 }
+
                 if (valEnd) {
                     $("#eventEndDate").text(formatDate(valEnd));
                     $("#eventEndTime").text(formatTime(valEnd));
                     $("#dateSeparator").removeClass("none");
                 } else {
-                    $("#eventEndDate").text("");
-                    $("#eventEndTime").text("");
+                    $("#eventEndDate, #eventEndTime").text("");
                     $("#dateSeparator").addClass("none");
                 }
-                if (valBtn == "") {
-                    $("#eventBtnDiv").addClass("none");
-                    $("#eventBtn").attr("href", "");
-                } else {
-                    $("#eventBtnDiv").removeClass("none");
-                    $("#eventBtn").attr("href", valBtn);
-                }
+
+                const btnState = valBtn === "" ? "add" : "remove";
+                $("#eventBtnDiv").toggleClass("none", btnState === "add");
+                $("#eventBtn").attr("href", valBtn || "");
             }
 
-            if (imgJson == "") {
-                $("#eventImg").addClass("none");
-
-                if ($("#eventModal").hasClass("calendar_update")) {
-                    $("#eventImg").removeClass("none");
-                }
-            } else {
-                $("#eventImg").removeClass("none");
-                let imgSrc = imgJson.replace("cross_asistent/", "");
-                $("#eventImg").attr("src", imgSrc);
+            $("#eventImg").toggleClass("none", imgJson === "" && !updateModal);
+            if (imgJson !== "") {
+                $("#eventImg").attr("src", imgJson.replace("cross_asistent/", ""));
             }
 
-            setTimeout(() => {
-                myModal.show();
-            }, 300);
+            setTimeout(myModal.show.bind(myModal), 300);
 
             info.jsEvent.preventDefault();
         },
