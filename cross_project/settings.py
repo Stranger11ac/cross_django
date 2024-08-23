272257aaf87a360cf29902ca6d442e806bbbdeb5
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2024-08-23 00:36:47 -0600

    change media root

--- cross_project/settings.py
+++ cross_project/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 # Para archivos subidos por los usuarios
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
 STATIC_URL = 'static/'
