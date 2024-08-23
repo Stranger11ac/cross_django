8401712fed91654c6387408c1a7928262e400dfb
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2024-08-23 15:06:10 -0600

    update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@
 set -o errexit
 
 # Instala las dependencias del sistema
-apt-get update && apt-get install -y portaudio19-dev
+apt-get update
+apt-get install -y portaudio19-dev || { echo "Failed to install portaudio19-dev"; exit 1; }
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+# Instalación de dependencias de Python
+pip install -r requirements.txt || { echo "Failed to install Python dependencies"; exit 1; }
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input || { echo "Failed to collect static files"; exit 1; }
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python manage.py migrate || { echo "Failed to apply database migrations"; exit 1; }
