d88f81a7d29b29a25fb7307d5aba75bf82acb543
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2024-08-23 15:12:46 -0600

    delete pyaudio

--- build.sh
+++ build.sh
@@ -2,15 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Instala las dependencias del sistema
-apt-get update
-apt-get install -y portaudio19-dev || { echo "Failed to install portaudio19-dev"; exit 1; }
-
-# Instalación de dependencias de Python
-pip install -r requirements.txt || { echo "Failed to install Python dependencies"; exit 1; }
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input || { echo "Failed to collect static files"; exit 1; }
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate || { echo "Failed to apply database migrations"; exit 1; }
+python manage.py migrate
