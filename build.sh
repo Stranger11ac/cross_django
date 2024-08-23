b8c8cc2418ff919feef3f415f5dfbdba07efb25f
Salvador alejandro hernandez cepeda <salvahdzcep@gmail.com>
2024-08-23 15:00:31 -0600

    install portaudio19

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Instala las dependencias del sistema
+apt-get update && apt-get install -y portaudio19-dev
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
